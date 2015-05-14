ba3835aa7f5bcc910e6d568d5773255fcb61b837
dmlaomao <dmlaomao@debian>
2015-05-14 16:39:01 +0800

    remove httpcontext since it's not necessary

--- src/DownloadPage.java
+++ src/DownloadPage.java
@@ -4,27 +4,23 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 
 public class DownloadPage implements Callable<String> {
     private String html;
     private final String url;
     private final CloseableHttpClient client;
-    private final HttpContext context;
     private final HttpGet httpget;
 
     public DownloadPage(CloseableHttpClient httpclient,String url) {
         this.client = httpclient;
-        this.context = new BasicHttpContext();
         this.httpget = new HttpGet(url);
         this.url = url;
     }
 
     private void getHTML() {
         try {
-            CloseableHttpResponse response = client.execute(httpget,context);
+            CloseableHttpResponse response = client.execute(httpget);
             try {
                 HttpEntity entity = response.getEntity();
                 if (entity != null) {
